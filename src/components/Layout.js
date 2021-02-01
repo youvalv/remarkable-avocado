6cf747aa4ca68a17639b1e4ca28a1c1fdcfdb4f6
Stackbit <projects@stackbit.com>
2021-02-01 12:49:59 +0000

    initial commit by Stackbit

---
+++ src/components/Layout.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import _ from 'lodash';
+
+import {withPrefix} from '../utils';
+import Header from './Header';
+import Footer from './Footer';
+
+export default class Body extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Helmet>
+                    <title>{_.get(this.props, 'page.frontmatter.title', null) && (_.get(this.props, 'page.frontmatter.title', null) + ' - ')}{_.get(this.props, 'data.config.title', null)}</title>
+                    <meta charSet="utf-8"/>
+                    <meta name="viewport" content="width=device-width, initialScale=1.0" />
+                    <meta name="description" content={_.get(this.props, 'page.frontmatter.excerpt', null)}/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
+                    {_.get(this.props, 'data.config.favicon', null) && (
+                    <link rel="icon" href={withPrefix(_.get(this.props, 'data.config.favicon', null))}/>
+                    )}
+                    <body className={'palette-' + _.get(this.props, 'data.config.palette', null)} />
+                </Helmet>
+                <div id="page" className="site">
+                  <Header {...this.props} />
+                  <main id="content" className="site-content">
+                    {this.props.children}
+                  </main>
+                  <Footer {...this.props} />
+                </div>
+            </React.Fragment>
+        );
+    }
+}
